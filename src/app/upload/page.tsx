c32a472512ef3476e17d182c4029f3ee8a5dacf2
Noctyris <noctyris@proton.me>
2025-07-07 22:59:11 +0200

    «Remove unused values

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -3,35 +3,53 @@
 import { CldUploadWidget } from "next-cloudinary";
 import { useState } from "react";
 
+type filter_t = {
+  type:     string;
+  exposure: number;
+  count:    number;
+}
+
 export default function Page() {
   const [resource, setResource] = useState();
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
+  const [scope, setScope] = useState('');
+  const [camera, setCamera] = useState('');
+  const [filters, setFilters] = useState<filter_t[]>([]);
+  const [stacking, setStacking] = useState('');
+  const [type, setType] = useState('');
+  const [status, setStatus] = useState('');
+
+  console.log(resource);
 
   return (
-    <>
+    <div>
       <h1 className="text-5xl">Upload</h1>
-      <CldUploadWidget
-        options={{
-          multiple: false,
-          sources: ["local"],
-          cropping: false,
-          clientAllowedFormats: ["webp", "png", "jpg", "jpeg"],
-        }}
-        signatureEndpoint="/api/sign-cloudinary-params"
-        onSuccess={(result, { widget }) => {
-          setResource(result?.info);
-        }}
-        onQueuesEnd={(result, { widget }) => {
-          widget.close();
-        }}
-      >
-        {({ open }) => {
-          function handleClick() {
-            setResource(undefined);
-            open();
-          }
-          return <button onClick={handleClick}>Upload an Image</button>;
-        }}
-      </CldUploadWidget>
-    </>
+      <div>
+	      <CldUploadWidget
+	        options={{
+	          multiple: false,
+	          sources: ["local"],
+	          cropping: false,
+	          clientAllowedFormats: ["webp", "png", "jpg", "jpeg"],
+	        }}
+	        signatureEndpoint="/api/sign-cloudinary-params"
+	        onSuccess={(result) => {
+	          setResource(result?.info);
+	        }}
+	        onQueuesEnd={({ widget }) => {
+	          widget.close();
+	        }}
+	      >
+	        {({ open }) => {
+	          function handleClick() {
+	            setResource(undefined);
+	            open();
+	          }
+	          return <button onClick={handleClick}>Upload an Image</button>;
+	        }}
+	      </CldUploadWidget>
+      </div>
+    </div>
   );
 }
