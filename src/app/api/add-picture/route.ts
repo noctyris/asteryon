8c97ca718a2ee25f1f830e2aa703764038d7bced
Noctyris <noctyris@proton.me>
2025-07-08 12:16:18 +0200

    Create add-picture api

---
+++ src/app/api/add-picture/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server';
+import sql from '@/app/lib/data';
+
+export async function POST(req: NextRequest) {
+        const data = await req.json();
+        console.log(data);
+
+        const { title, publicID, capture_date, scope, camera, filters, stacking, type } = data;
+
+        try {
+                await sql`
+                        INSERT INTO pictures (title, "publicID", capture_date, scope, camera, filters, stacking, type)
+                        VALUES (
+                                ${title},
+                                ${publicID},
+                                ${capture_date},
+                                ${scope},
+                                ${camera},
+                                ${filters ? sql.json(filters) : null},
+                                ${stacking},
+                                ${type}
+                        )
+                `;
+                return NextResponse.json({ success: true });
+        } catch (err) {
+                console.error(err);
+                return NextResponse.json({ error: 'Database insert failed' }, { status: 500 });
+        }
+}
