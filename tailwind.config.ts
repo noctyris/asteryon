7b054a95c9656a09817414bfc5d7ae6ce4ac3785
Noctyris <noctyris@proton.me>
2025-07-09 17:09:52 +0200

    Create tailwind config file

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          DEFAULT: "#6366f1",
+          dark: "#4f46e5",
+          light: "#818cf8",
+        },
+      },
+    },
+  },
+  safelist: [],
+  plugins: [],
+} satisfies Config;
