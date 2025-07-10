956e40b76626b22fdacd743772029ffd8cd93313
Noctyris <noctyris@proton.me>
2025-07-10 21:48:28 +0200

    Add Picture type to fetchImages function

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,8 +1,9 @@
 import postgres from "postgres";
+import { Picture } from "@/types";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
-export async function fetchImages() {
+export async function fetchImages(): Promise<Picture[]> {
   return await sql`SELECT * FROM pictures ORDER BY capture_date DESC`;
 }
 
