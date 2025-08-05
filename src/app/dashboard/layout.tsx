606153ddd740c5d5df2c550ed5e7bfdbe6483560
Noctyris <noctyris@proton.me>
2025-08-05 15:19:31 +0200

    Maybe fix session bug

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import { SessionProvider } from 'next-auth/react';
+import { auth } from "@root/auth"
+
+export default async function DashboardLayout({ children } : Readonly<{children: React.ReactNode;}>) {
+  const session = await auth();
+  console.log(session);
+
+  return (
+    <>
+      <p>dashbaord</p>
+      <SessionProvider session={session}>{children}</SessionProvider>
+    </>
+  )
+}
